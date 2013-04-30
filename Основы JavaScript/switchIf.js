cc24aa97de9697db803cb0dc1324464b762070d2
bogdan <onikiienko@gmail.com>
2013-04-30 17:10:53 +0300

    lesson /"switch/"

--- Основы JavaScript/switchIf.js
+++ Основы JavaScript/switchIf.js
@@ -1,10 +1,18 @@
 var browser = prompt("Ваш браузер", "");
-if (browser=="IE"){
-    alert('О, да у вас ИЕ!');
-    break;
-}else if (browser=="chrome"||"firefox"||"Safari"||"opera"){
-    alert('Да, и эти браузеры мы поддержаваем');
-    break;
-} else{
-    alert('Мы надеемся, что и в вашем браузере все ок!');
+(browser=="IE")?alert('О, да у вас ИЕ!'):
+    (browser=="chrome"||browser=="firefox"||browser=="safari"||browser=="opera")?alert('Да, и эти браузеры мы поддержаваем'):
+       alert('Мы надеемся, что и в вашем браузере все ок!');
+
+var a = prompt('a?','');
+
+switch(a){
+    case '0':
+        alert('0');
+        break;
+    case '1':
+        alert('1');
+        break;
+    case '2':
+    case '3':
+        alert('2,3');
 }
