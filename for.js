6f5f0827af4156454bfd9ed502a06fbe93281020
bogdan <onikiienko@gmail.com>
2013-05-13 17:13:34 +0300

    deleted non-forward files

--- for.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: onikienko
- * Date: 4/28/13
- * Time: 11:38 PM
- * To change this template use File | Settings | File Templates.
- */
-for(;;){}
--- promts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: onikienko
- * Date: 4/26/13
- * Time: 10:42 PM
- * To change this template use File | Settings | File Templates.
- */
-var name = prompt('your name is', '');
-alert(name);
