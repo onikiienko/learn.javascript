c8d257189b977cd7fc9180bfd434c0722632d100
bogdan <onikiienko@gmail.com>
2013-05-23 19:12:49 +0300

    finaly finished the data structures

--- Структуры данных/arraysHomeWork.js
+++ Структуры данных/arraysHomeWork.js
@@ -51,3 +51,41 @@ a[0] = 5;
 
 //alert(arr[0]);
 //alert(a[0]);
+
+
+var store = { }; // объект для коллекции
+
+var items = ["div", "a", "form"];
+
+for(var i=0; i<items.length; i++) {
+    var key = items[i]; // для каждого элемента создаём свойство
+    store[ key ] = true; // значение здесь не важно
+}
+
+//Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+function aclean(arr) {
+    // этот объект будем использовать для уникальности
+    var obj = {};
+
+    for(var i=0; i<arr.length; i++) {
+        // разбить строку на буквы, отсортировать и слить обратно
+
+        var sorted = arr[i].toLowerCase().split('').sort().join(''); // (*)
+
+
+        obj[sorted] = arr[i]; // сохраняет только одно значение с таким ключом
+    }
+
+    var result = [];
+
+    // теперь в obj находится для каждого ключа ровно одно значение
+    for(var key in obj) result.push(obj[key]);
+
+    return result;
+}
+
+var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
+
+alert( aclean(arr) );
+
--- Структуры данных/date.js
+++ Структуры данных/date.js
@@ -8,19 +8,20 @@
 //Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут.
 //Временная зона — местная. Выведите его на экран.
 var d = new Date(2012, 1, 20, 3, 12);
-console.log(d);
+//console.log(d);
 
 var date = new Date();
 
 
-console.log( date.getHours() ); // час в вашей зоне для даты date
-console.log( date.getUTCHours() , new Date().getTimezoneOffset()); // час в зоне GMT+0 для даты date
+//console.log( date.getHours() ); // час в вашей зоне для даты date
+//console.log( date.getUTCHours() , new Date().getTimezoneOffset()); // час в зоне GMT+0 для даты date
 
 
 //Создайте функцию getWeekDay(date), которая выводит текущий день недели в коротком формате ‘пн’, ‘вт’, … ‘вс’.
 
-var date = new Date(2012,0,3);  // 3 января 2012
-console.log( getWeekDay(date) );      // Должно вывести 'вт'
+var date = new Date;  // 3 января 2012
+date.setDate(-100);
+//console.log( getWeekDay(date) );      // Должно вывести 'вт'
 
 function getWeekDay(date) {
     var day = date.getDay();
@@ -29,11 +30,128 @@ function getWeekDay(date) {
 
 
 //Напишите функцию, getLocalDay(date) которая возвращает день недели для даты date.
-var date = new Date(2012, 0, 3);  // 3 янв 2012
-console.log( getLocalDay(date) );      // вторник, выведет 2
+var date = new Date;  // 3 янв 2012
+//console.log( date );      // вторник, выведет 2
+//console.log( getLocalDay(date) );      // вторник, выведет 2
 
 function getLocalDay(date) {
     var v = date.getDay();
     return (v == 0) ? 7 : v;
 }
 
+
+var today = new Date;
+
+today.setHours(0);
+//console.log( today ); // сегодня, но час изменён на 0
+
+today.setHours(0, 0, 0, 0);
+//console.log(today ); // сегодня, ровно 00:00:00.
+
+
+//console.log(getLastDayInMonth(2012, 1)); // 29 (високосный год, февраль).
+
+function getLastDayInMonth(year, month) {
+    var d = new Date(year, month + 1, 0);
+    return d;
+}
+
+/*Напишите код, который выводит:
+
+    Сколько секунд прошло с начала сегодняшнего дня.
+    Сколько осталось до конца дня.
+    Скрипт должен работать в любой день, т.е. в нём не должно быть конкретного значения сегодняшней даты.
+*/
+
+//console.log(getTimeFromSunRise());
+//console.log(getTimeBeforeSunSet());
+
+function getTimeFromSunRise() {
+    var date = new Date;
+    return date.getMilliseconds() + 1000 * ( date.getSeconds() + 60 * ( date.getMinutes() + date.getHours() * 60 ) );
+}
+
+function getTimeBeforeSunSet() {
+    return 86400000 - getTimeFromSunRise();
+}
+
+var now = new Date();
+
+// создать объект из текущей даты, без часов-минут-секунд
+var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+var diff = now - today; // разница в миллисекундах
+//console.log( Math.round(diff / 1000) ); // перевести в секунды
+
+// Напишите функцию formatDate(date), которая выводит дату date в формате дд.мм.гг:
+// P.S. Обратите внимание, ведущие нули должны присутствовать, то есть 1 января 2011 должно быть 01.01.11, а не 1.1.11.
+
+var d = new Date(2001, 0, 30); // 30 января 2011
+//console.log( formatDate(d) ); // '30.01.11'
+
+function formatDate(d) {
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+
+    year = year % 100;
+    if (year < 10) {
+        year = '0' + year;
+    }
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    return day + '.' + month + '.' + year;
+}
+
+/*
+Напишите функцию formatDate(date), которая форматирует дату date так:
+
+    Если со времени date прошло менее секунды, то возвращает "только что".
+    Иначе если со времени date прошло менее минуты, то "n сек. назад".
+    Иначе если прошло меньше часа, то "m мин. назад".
+    Иначе полная дата в формате "дд.мм.гг чч:мм".
+*/
+
+
+console.log( formatDate( new Date(new Date - 1) ) ); // "только что"
+
+console.log( formatDate( new Date(new Date - 30*1000) ) ); // "30 сек. назад"
+
+console.log( formatDate( new Date(new Date- 5*60*1000) ) ); // "5 мин. назад"
+
+console.log( formatDate( new Date(new Date - 86400*1000) ) ); // вчерашняя дата в формате "дд.мм.гг чч:мм"
+
+function formatDate(date) {
+    var diff = new Date() - date; // разница в миллисекундах
+
+    if (diff < 1000) { // прошло менее 1 секунды
+        return 'только что';
+    }
+
+    var sec = Math.floor( diff / 1000 ); // округлить diff до секунд
+
+    if (sec < 60) {
+        return sec + ' сек. назад';
+    }
+
+    var min = Math.floor( diff / 60000 ); // округлить diff до минут
+    if (min < 60) {
+        return min + ' мин. назад';
+    }
+
+    // форматировать дату, с учетом того, что месяцы начинаются с 0
+    var d = date;
+    d = ['0'+d.getDate(),'0'+(d.getMonth()+1),''+d.getFullYear(), '0'+d.getHours(),'0'+d.getMinutes() ];
+    for(var i=0; i<d.length; i++) {
+        d[i] = d[i].slice(-2);
+    }
+
+    return d.slice(0,3).join('.')+' '+d.slice(3).join(':');
+}
+
+//console.log( "" + 1 + 0, "" - 1 + 0, true + false, 6 / "3", "2" * "3", 4 + 5 + "px", "$" + 4 + 5 , "4" - 2 , "4px" - 2 , 7 / 0 , parseInt("09") , "  -9\n" + 5, "  -9\n" - 5, 5 && 2 && 123 , 2 && 5 , 5 || 0 , 0 || 5, null + 1, undefined + 1);
